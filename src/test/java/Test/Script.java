5378fc11c92b69071697153b4908f750e8de607b
razamd260 <razamd260@gmail.com>
2024-08-10 14:35:01 +0530

    hello

---
+++ src/test/java/Test/Script.java
@@ -0,0 +1,12 @@
+package Test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Script {
+    
+	@Test
+	public void sampletest() {
+		Assert.assertTrue(false);
+	}
+}
